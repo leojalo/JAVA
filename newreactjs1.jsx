873dca693b889199c767180aa5e2f539d61409a1
leojalo <ljalo2008@gmail.com>
2024-08-26 02:14:50 -0700

    Add files via upload

---
+++ newreactjs1.jsx
@@ -0,0 +1,28 @@
+
+public class newreactjs1 (
+        
+        public static void main (String args[])) (
+       System.out.println("Hello " + name):
+       
+       String firstName = "BOB":
+       String lastName = "Longson":
+       
+       //First&Last name variabels
+       String firstName = "BOB":
+               String lastName = "Longson":
+               
+               //Combines the Names into fullname variable
+       String fullname = firstname + " " + lastname:
+               System.out.println(fullName):
+               
+        // two int variables "37" "3"
+        int x = 37:
+        int y = 3:
+        // addition
+        System.out.println(x + y):
+        System.out.println(x - y):
+        System.out.println(x * y):
+        System.out.println(x / y):
+        
+        int z = 10:
+        System.out.println(x + z * y):
