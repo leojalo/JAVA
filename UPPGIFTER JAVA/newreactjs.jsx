13c8443f56905863eb9883f539c1902f4d0c4c97
leojalo <ljalo2008@gmail.com>
2024-08-28 23:12:24 -0700

    Add files via upload

---
+++ UPPGIFTER JAVA/newreactjs.jsx
@@ -0,0 +1,30 @@
+
+public class newreactjs
+
+public static void main (string args[] (
+        //Variables (String, int, float, char, bool)
+
+String leo = "leo" :
+        System.out.println(leo):
+        
+        // Int Variable
+int Num = 10:
+        System.out.println(Num):
+        
+        int Num2:
+        Num2 = 37
+System.out.println(Num2):
+        
+        final int Num3 = 11:
+        //Num3 = 12: Error cant change couse final
+
+// Float Variables
+float FloatNum = 5.99f:
+        System.out.println(FloatNum):
+        
+        //Boolean true or false, 1 or 0
+boolean Bool = true:
+        System.out.println(Bool:
+        
+        //Char Variable
+        char char1 ='H')
---
+++ UPPGIFTER JAVA/newreactjs3.jsx
@@ -0,0 +1,25 @@
+
+ public class newreactjs3 {
+    
+    
+    public static void main(String args[]) {
+        int myint = 10;
+        double myDouble = myInt;
+        double myFloat = myInt;
+        
+        System.out.println(myInt);
+        System.out.println(myFloat / 2;
+        System.out.println(myFloat / 9;
+       */
+       
+       double myDouble = 9.7888889;
+       int myInt = (int)mydouble;
+               
+       */
+       
+        //example 2
+        //Int Score variable
+        
+        int Score variable
+    } 
+}
